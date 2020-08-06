7af8d354c80ca18b7c8d6a5419d6e9d6e81ce462
fahre3koma2 <fahre.tigakomadua@gmail.com>
2020-08-06 21:19:35 +0700

    AdminLTE commit migration

---
+++ database/migrations/2020_08_06_133321_create_profil_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfilTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profil', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nama', 100)->nullable();
+            $table->string('email', 45)->nullable();
+            $table->string('foto', 45)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profil');
+    }
+}
